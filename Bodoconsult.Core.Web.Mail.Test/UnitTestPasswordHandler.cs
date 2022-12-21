8de0b964fa897a9d9b0e3dd8079b20948bab5dcd
Robert Leisner <robert.leisner@bodoconsult.de>
2022-12-21 18:54:50 +0100

    [MOD] Altered unit test

--- Bodoconsult.Core.Web.Mail.Test/UnitTestPasswordHandler.cs
+++ Bodoconsult.Core.Web.Mail.Test/UnitTestPasswordHandler.cs
@@ -18,12 +18,12 @@ namespace Bodoconsult.Core.Web.Mail.Test
         {
             var pwdKey = new O365MailAccount()
             {
-                Tenant = "F/9NSz5BmzPjSUpRrLM9wcKu1NmwWSdR8SuVPyIfrk38E2BdLVgDv+28Z8tWJcRnSIRnMP8TAeDuZddJS+N0pnqc01s7L4PzpjOZ4LbKMII=",
-                Instance = "mZqrt2SMI5qC5SHZgN4H8H1SC4w6ZJ5IvRbtXbif1wHvv/TsHGfwED0xyZL3JfRzpANnV2Og8WDKHPmwIONr9zjIrNqrEKh+E5ryrFlWCiM=",
-                Scope = "mZqrt2SMI5qC5SHZgN4H8Pl5xFwFcdDz0SNOQapGrOEZhuEBuVhsem/GOpp4iQ774y+cHye7RHysJ1aZfLhrOo033UG0KRVtEPpx/6VfVsQ=",
-                ClientId = "EuXih/XyadFXuM7Sy64/c0VMF+iNEYIiMQD3AErwDWya5RVmQiKVXdPYR8XcHFW1N2cx3I/Ew8UthbHnUrBGki7QdF5Pu21yGjenRhU9+7A=",
-                ClientSecret = "Q+x1I8jq3Yd6n/mANjOQ+VGBSRwKbpFttHVfjQA/Wt85ku4tgBM5FSIW9r5MoRBwXVNXwmthOdtzlJlZ8oms68Fzdau952LuZrYxeKj/uJDrc5uBrjZBwdSfOnKdJnbM",
-                UserName = "1dHEZnNSGo+o3gWlMT1ygybJrVIwzWEGlvAbQldjcRHuCNcOa73i+6o0nEO1Y2lYPRYhdGkjqU3sICcgnv0HRLfUFxFPvawFqwgikw7qT9I=",
+                Tenant = "",
+                Instance = "",
+                Scope = "",
+                ClientId = "",
+                ClientSecret = "",
+                UserName = "",
 
             };
 
